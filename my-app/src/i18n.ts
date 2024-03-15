b26bdf4b5f9fe652c7b8d734dd46602bac07fa32
maliga <wojciech.maliga@nordcloud.com>
2024-03-15 12:37:21 +0100

    React i18next languages added

--- my-app/src/i18n.ts
+++ my-app/src/i18n.ts
@@ -1,15 +1,19 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
+import en from './translations/en.json';
+import de from './translations/de.json';
+import fi from './translations/fi.json';
+import pl from './translations/pl.json';
+
 i18n
   .use(initReactI18next)
   .init({
     resources: {
-      en: {
-        translation: {
-          'my-app': 'My Application',
-        }
-      }
+      en: { ...en },
+      de: { ...de },
+      fi: { ...fi },
+      pl: { ...pl }
     },
     fallbackLng: 'en',
   });
