95f484c205361293e2968f186bd767b7c50a0544
maliga <wojciech.maliga@nordcloud.com>
2024-03-15 12:52:55 +0100

    i18next language detector added

--- my-app/src/i18n.ts
+++ my-app/src/i18n.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 import en from './translations/en.json';
 import de from './translations/de.json';
@@ -8,6 +9,7 @@ import pl from './translations/pl.json';
 
 i18n
   .use(initReactI18next)
+  .use(LanguageDetector)
   .init({
     resources: {
       en: { ...en },
